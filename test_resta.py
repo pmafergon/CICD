8021fc831e3c536fc6498de28e7cda9726724c28
Pedro Manuel Fernández <136108943+pmafergon@users.noreply.github.com>
2024-03-13 23:09:35 +0100

    'test3'

--- test_resta.py
+++ test_resta.py
@@ -7,5 +7,6 @@ class TestRestar(unittest.TestCase):
     self.assertEqual(restar(3, 2), 1)
     self.assertEqual(restar(-1, 1), -2)
     self.assertEqual(restar(-1, -1), 0)
+    
 if __name__ == '__main__':
  unittest.main()
